5313cc368d3496e7d76b7e7d6bc07e9ed69c53fe
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-10-19 12:10:31 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Origin', 'X-Requested-With', 'Authorization', 'Content-Type', 'Accept', 'x-custom-header'],
+    'allowed_headers' => ['Origin', 'X-Requested-With', 'Authorization', 'Set-Cookie', 'Content-Type', 'Accept', 'x-custom-header'],
     'exposed_headers' => ['x-custom-response-header'],
     'max_age' => 60,
     'supports_credentials' => true
